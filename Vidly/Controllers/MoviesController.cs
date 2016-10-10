c18ac6768d747333ba04b9fe203f28d4a0b34082
vchavan <vishal.chavann@gmail.com>
2016-10-10 12:21:13 +0200

    Migrated.

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -13,28 +13,41 @@ namespace Vidly.Controllers
 
         public ActionResult Random()
         {
-            var movies = new Movie() { Name = "Shrek!!!" };
-
-            var Customers = new List<Customers>
-            {
-                new Customers { Name = "Customer 1"},
-                new Customers { Name = "Customer 2"},
-                new Customers { Name = "Customer 3"},
-                new Customers { Name = "Customer 4"},
-                 new Customers { Name = "Customer 5"}
-            };
-
-
-            var MovieViewModel = new RandomMovieViewModel
-            {
-                Movie = movies,
-                Customers = Customers
-            };
-
-            return View(movies);
-
+            var objMovie = GetMovie();
+            return View(objMovie);
         }
 
+        private IEnumerable<Movie> GetMovie()
+        {
+
+            return new List<Movie>
+            {
+                new Movie {ID=0, Name="Movie 1"},
+                new Movie {ID=1, Name="Movie 2"},
+                new Movie {ID=2, Name="Movie 3"},
+                new Movie {ID=3, Name="Movie 4"},
+                new Movie {ID=4,Name="Movie 5"},
+                new Movie {ID=5,Name="Movie 6"}
+
+            };
+        }
+
+        public ActionResult Details(int ID)
+        {
+            var customerDet = GetMovie().SingleOrDefault(c => c.ID.Equals(ID));
+            
+
+            if (customerDet != null)
+            {
+                return View(customerDet);
+               
+            }
+            else
+            {
+                ModelState.AddModelError("Error", "Specified row does not exist.");
+                return Content("Not Exist!!!");
+            }
+        }
 
     }
 }
